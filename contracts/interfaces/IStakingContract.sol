337a7751874df115523f9bf9b0b51d78f81db44b
Quintin <quintinmakwe@gmail.com>
2021-11-01 12:44:40 +0000

    project setup, interface definition and library initialization

---
+++ contracts/interfaces/IStakingContract.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.0;
+
+interface IStakingContract {
+    function stake(
+        address account,
+        uint256 amount,
+        address stakingTokenAddress
+    ) external returns (uint256);
+
+    function unstake(address account, uint256 stakeId)
+        external
+        returns (uint256);
+
+    function viewUnstakableToken(address recipient)
+        external
+        view
+        returns (uint256);
+
+    function distributeReward(uint256 amount) external;
+}
