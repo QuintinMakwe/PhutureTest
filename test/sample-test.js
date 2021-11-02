c61553fce7815d333c4eca7e9e06f359cb61d87d
Quintin <quintinmakwe@gmail.com>
2021-11-02 00:52:16 +0000

    updated test for staking action

--- test/sample-test.js
+++ test/sample-test.js
@@ -3,7 +3,10 @@ const { ethers } = require("hardhat");
 const web3 = require("web3");
 
 describe("Staking", function () {
+  let owner 
+  let stakeAmount =  web3.utils.toWei("10", "ether")
   before(async function() {
+    owner = (await ethers.getSigners())['0'].address;
     TKNToken = await ethers.getContractFactory('TKNToken');
     tKNToken= await TKNToken.deploy(web3.utils.toWei('100000000', "ether"))
     await tKNToken.deployed();
@@ -11,52 +14,58 @@ describe("Staking", function () {
     Staking = await ethers.getContractFactory('Staking');
     staking = await Staking.deploy(tKNToken.address);
     await staking.deployed();
+
+    await tKNToken.approve(staking.address, stakeAmount);
+    await staking.stake(owner);
   })
 
-  it("should initialize aggregate staking amount to zero", async function(){
-    console.log('running here')
-    expect((await staking._totalAccruedReward()).toString()).to.equal('0')
+  it("should update contract balance correctly", async function(){
+    expect((await tKNToken.balanceOf(staking.address)).toString()).to.equal(stakeAmount)
   })
 
-  // it("updates total aggregate stake correctly")
-  // it("updates ")
+  it("should check that total aggregate stake for actor updated correctly", async function(){
+    expect((await staking._aggregateStakeAmount(owner)).toString()).to.equal(stakeAmount)
+  })
+  it("should create stake index", async function(){
+    expect((await staking._stakeIndexes(owner, 1)).exists).to.equal(true)
+  })
+  it("should update reward snapshot correctly", async function() {
+    expect((await staking._rewardSnapshot(owner, 1))).to.equal('0')
+  });
+  it("should update individual stake correctly", async function() {
+    expect((await staking._aggregateStakeAmount(owner)).toString()).to.equal(stakeAmount);
+  });
 });
 
 
-describe("Unstaking", function () {
-  before(async function() {
-    TKNToken = await ethers.getContractFactory('TKNToken');
-    tKNToken= await TKNToken.deploy(web3.utils.toWei('100000000', "ether"))
-    await tKNToken.deployed();
+// describe("Unstaking", function () {
+//   before(async function() {
+//     TKNToken = await ethers.getContractFactory('TKNToken');
+//     tKNToken= await TKNToken.deploy(web3.utils.toWei('100000000', "ether"))
+//     await tKNToken.deployed();
 
-    Staking = await ethers.getContractFactory('Staking');
-    staking = await Staking.deploy(tKNToken.address);
-    await staking.deployed();
-  })
+//     Staking = await ethers.getContractFactory('Staking');
+//     staking = await Staking.deploy(tKNToken.address);
+//     await staking.deployed();
+//   })
 
-  it("checks", async function(){
-    console.log('running here')
-    expect((await staking._totalAccruedReward()).toString()).to.equal('0')
-  })
-});
+//   it("should check that total amount in contract decreased correctly");
+//   it("should check that stake index doesn't exist");
+//   it("should check the total aggregated stakes for the user updated correctly");
+//   it("should throw an error when wrong stake index is supplied");
+// });
 
 
-describe("Distribution", function () {
-  before(async function() {
-    TKNToken = await ethers.getContractFactory('TKNToken');
-    tKNToken= await TKNToken.deploy(web3.utils.toWei('100000000', "ether"))
-    await tKNToken.deployed();
+// describe("Distribution", function () {
+//   before(async function() {
+//     TKNToken = await ethers.getContractFactory('TKNToken');
+//     tKNToken= await TKNToken.deploy(web3.utils.toWei('100000000', "ether"))
+//     await tKNToken.deployed();
 
-    Staking = await ethers.getContractFactory('Staking');
-    staking = await Staking.deploy(tKNToken.address);
-    await staking.deployed();
-  })
+//     Staking = await ethers.getContractFactory('Staking');
+//     staking = await Staking.deploy(tKNToken.address);
+//     await staking.deployed();
+//   })
 
-  it("should initialize aggregate staking amount to zero", async function(){
-    console.log('running here')
-    expect((await staking._totalAccruedReward()).toString()).to.equal('0')
-  })
-
-  it("updates total aggregate stake correctly")
-  it("updates ")
-});
+//   it("should check that total accrued reward on the system updated correctly");
+// });
